2988accd049b9a309524ff76261ad75056c8783c
madhumithaR <30334068+madhumithaR@users.noreply.github.com>
2017-07-21 16:32:19 +0530

    factoriel.c

---
+++ factoriel.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int fact,i;
+fact=1;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<n;i++)
+{
+fact=fact*i;
+}
+printf("the factoriel of i is  %d",n,fact);
+}
